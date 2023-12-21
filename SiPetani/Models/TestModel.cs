055913eae3649f52f5f03479da07e05ec3e3ce2e
Gudbai <aldienmaulana@gmail.com>
2023-12-22 01:12:03 +0700

    Early Access

---
+++ SiPetani/Models/TestModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace SiPetani.Models
+{
+    public class TestModel
+    {
+        public int StatusID { get; set; }
+        public string StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+    }
+}
